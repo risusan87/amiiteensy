8e73ce60969eb3819bcd530e595a10fcf01736b8
rkoba0020 <rkoba0020@gmail.com>
2025-05-15 19:50:19 -0400

    Setup FlexRAM, VT pointer, and USB1 peripheral

--- teensy4/startup.c
+++ teensy4/startup.c
@@ -6,61 +6,45 @@ extern int main(void);
 extern uint32_t _sitcm, _itcm_start, _itcm_end;
 extern uint32_t _sdata, _dtcm_data_start, _dtcm_data_end;
 extern uint32_t _sbss, _ebss;
+extern void (* const VectorTable[])(void);
 
-void uart_init(void);
-void uart1_putchar(char c);
-char uart1_getchar(void);
+// peripherals
+extern void usb1_init(void);
 
 __attribute__((section(".startup"), used))
 void ResetHandler(void)
 { 
+    // FlexRAM allocation
+    // RAM size = 512KB, total 16 banks of 32KB section
+    // 0b00 = not used, 0b01 = OCRAM, 0b10 = DTCM, 0b11 = ITCM
+    // 0b11111111111111111010101001010101
+    // 0x   F   F   F   F   A   A   5   5 
+    // TODO: Dynamic configiration within linker script
+    IOMUXC_GPR_GPR16 |= (1 << 2);
+    IOMUXC_GPR_GPR17 = 0xFFFFAA55;
+    // Unknown purpose but this will write into reserved bits
+    // IOMUXC_GPR_GPR14 = 0x00AA0000;
+
+    // Vector table pointer set
+    SCB_VTOR = (uint32_t) &VectorTable;
+
     // move executable code to ITCM
+    // due to the limited space, prioritize ISRs!
     memcpy(&_itcm_start, &_sitcm, (size_t)&_itcm_end - (size_t)&_itcm_start);
+
+    // Both data and bss are intended to be in DTCM
     // move .data to DTCM
     memcpy(&_dtcm_data_start, &_sdata, (size_t)&_dtcm_data_end - (size_t)&_dtcm_data_start);
     // clear .bss
     memset(&_sbss, 0, (size_t)&_ebss - (size_t)&_sbss);
 
-    // Enable UART1
-    uart_init();
+    // GPIO B0_03 (PIN 13) for output
+    IOMUXC_GPR_GPR27 = 0xFFFFFFFF;
+    GPIO7_GDIR |= (1 << 3);
+    
+    // enable USB OTG 1 for CDC
+    usb1_init();
 
-    print("Hello, world!\n");
-    print("Press any key to continue...\n");
+    
     main();
-}
-
-void uart_init(void)
-{
-    // LPUART1 clock enable
-    CCM_CCGR5 |= CCM_CCGR5_LPUART1(CCM_CCGR_ON);
-    // LPUART1 TX/RX sets to ALT2
-    IOMUXC_SW_MUX_CTL_PAD_GPIO_AD_B0_12 = 2;
-    IOMUXC_SW_MUX_CTL_PAD_GPIO_AD_B0_13 = 2;
-
-    LPUART1_CTRL &= ~(1 << 18);
-    LPUART1_CTRL &= ~(1 << 19);
-
-    LPUART1_BAUD = 0xE;
-
-    LPUART1_CTRL = 0;
-
-    LPUART1_CTRL |= (1 << 18) | (1 << 19);
-
-}
-
-void uart1_putchar(char c) {
-    while (!(LPUART1_STAT & (1 << 23))); // wait for TXEMPT
-    LPUART1_DATA = c;
-}
-
-char uart1_getchar(void) {
-    while (!(LPUART1_STAT & (1 << 21))); // wait for RDRF
-    return LPUART1_DATA;
-}
-
-void print(const char *s) {
-    while (*s) {
-        if (*s == '\n') uart1_putchar('\r'); // optional CR before LF
-        uart1_putchar(*s++);
-    }
 }
