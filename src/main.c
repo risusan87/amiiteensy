11f109bd9c134e5c4f5fbb87ba546c6da86c7ab8
rkoba0020 <rkoba0020@gmail.com>
2025-05-15 19:48:47 -0400

    Edit main

--- src/main.c
+++ src/main.c
@@ -14,20 +14,14 @@ void delay(int duration) {
 
 int main()
 {
-    // Configure GPIO B0_03 (PIN 13) for output
-    IOMUXC_SW_MUX_CTL_PAD_GPIO_B0_03 = 5;
-    IOMUXC_SW_PAD_CTL_PAD_GPIO_B0_03 = IOMUXC_PAD_DSE(7);
-    IOMUXC_GPR_GPR27 = 0xFFFFFFFF;
-    GPIO7_GDIR |= (1 << 3);
 
     for(;;) {
-
         // HIGH
-        GPIO7_DR_SET = (1 << 3);
-        delay(DELAY_LENGTH);
+    GPIO7_DR_SET = (1 << 3);
+    delay(10000000);
 
-        // LOW
-        GPIO7_DR_CLEAR = (1 << 3);
-        delay(DELAY_LENGTH);
+    // LOW
+    GPIO7_DR_CLEAR = (1 << 3);
+    delay(10000000);
     }
 }
