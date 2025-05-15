c489a5f68456ff5cd0aa1133e6ddb51748f01389
rkoba0020 <rkoba0020@gmail.com>
2025-05-15 19:48:37 -0400

    Edit py

--- serial_monitor.py
+++ serial_monitor.py
@@ -5,7 +5,7 @@ ports = serial.tools.list_ports.comports()
 for port in ports:
     print(port.device, port.description)
 
-ser = serial.Serial(port='/dev/cu.usbmodem122533301', baudrate=8795, timeout=1)
+# ser = serial.Serial(port='/dev/cu.usbmodem122533301', baudrate=8795, timeout=1)
 
-while True:
-    print("USB CDC:")
+# while True:
+#     print("USB CDC:")
