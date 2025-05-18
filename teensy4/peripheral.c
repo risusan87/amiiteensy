65c700a47684b37d835f8275384db0f3564e52b6
rkoba0020 <rkoba0020@gmail.com>
2025-05-18 13:16:16 -0400

    Add ISR type - URI and UI (not completed)

--- teensy4/peripheral.c
+++ teensy4/peripheral.c
@@ -1,59 +1,96 @@
 #include "imxrt.h"
 #include "utils.h"
 
-struct {
-        uint32_t next;
-        volatile uint32_t status;
-        uint32_t pointer0;
-        uint32_t pointer1;
-        uint32_t pointer2;
-        uint32_t pointer3;
-        uint32_t pointer4;
-        uint32_t callback_param;
-} transfer_t;
+// Device transfer descriptor (42.5.5.2 page 2396)
+// must be aligned to 32 bytes
+typedef struct __attribute__((aligned(32)))
+{
+    uint32_t next_link_pointer; // next dTD
+    uint32_t status;
+    uint32_t buffer0;
+    uint32_t buffer1;
+    uint32_t buffer2; // reserved
+    uint32_t buffer3; // reserved
+    uint32_t buffer4; // reserved
+    uint32_t reserved; // reserved
+} dtd_t;
 
-typedef struct {
+// typedef struct {
+//         uint32_t next;
+//         volatile uint32_t status;
+//         uint32_t pointer0;
+//         uint32_t pointer1;
+//         uint32_t pointer2;
+//         uint32_t pointer3;
+//         uint32_t pointer4;
+//         uint32_t callback_param;
+// } transfer_t;
+
+// 42.5.5.1 page 2394
+// TODO: union this for better complehension
+typedef struct __attribute__((aligned(64))) {
 	uint32_t config;
-	uint32_t current;
-	uint32_t next;
+	dtd_t *current_dtd;
+	dtd_t *next_dtd;
 	uint32_t status;
-	uint32_t pointer0;
-	uint32_t pointer1;
-	uint32_t pointer2;
-	uint32_t pointer3;
-	uint32_t pointer4;
+	uint32_t buffer0;
+	uint32_t buffer1;
+	uint32_t buffer2;
+	uint32_t buffer3;
+	uint32_t buffer4;
 	uint32_t reserved;
-	uint32_t setup0;
-	uint32_t setup1;
-	struct transfer_t *first_transfer;
-	struct transfer_t *last_transfer;
-	void (*callback_function)(struct transfer_t *completed_transfer);
-	uint32_t unused1;
-} endpoint_t;
+	uint32_t setup0; 
+    uint32_t setup1; 
+} endpoint_qh_t;
 
-union {
-    struct {
-     union {
-      struct {
-           uint8_t bmRequestType;
-           uint8_t bRequest;
-      };
-           uint16_t wRequestAndType;
-     };
-           uint16_t wValue;
-           uint16_t wIndex;
-           uint16_t wLength;
+// typedef struct {
+// 	uint32_t config;
+// 	uint32_t current;
+// 	uint32_t next;
+// 	uint32_t status;
+// 	uint32_t pointer0;
+// 	uint32_t pointer1;
+// 	uint32_t pointer2;
+// 	uint32_t pointer3;
+// 	uint32_t pointer4;
+// 	uint32_t reserved;
+// 	uint32_t setup0;
+// 	uint32_t setup1;
+// 	transfer_t *first_transfer;
+// 	transfer_t *last_transfer;
+// 	void (*callback_function)(transfer_t *completed_transfer);
+// 	uint32_t unused1;
+// } endpoint_qh_t;
+
+typedef union 
+{
+    struct 
+    {
+        union 
+        {
+            struct 
+            {
+                uint8_t bmRequestType;
+                uint8_t bRequest;
+            };
+            uint16_t wRequestAndType;
+        };
+        uint16_t wValue;
+        uint16_t wIndex;
+        uint16_t wLength;
     };
-    struct {
+    struct 
+    {
            uint32_t word1;
            uint32_t word2;
     };
        uint64_t bothwords;
 } setup_t;
 
-// endpoint queue head must be aligned to 4096 bytes
-__attribute__((aligned(4096))) 
-static endpoint_t queue_head_list[16];
+// endpoint queue head must be aligned to 2048 bytes (42.5.5 page 2394)
+// num of endponts = 8, each endpoint has 2 queue heads (IN/OUT) (42.5.6.3)
+__attribute__((aligned(2048))) 
+static endpoint_qh_t queue_head_list[8][2];
 
 __attribute__((section(".startup"), used))
 void usb1_init(void)
@@ -94,9 +131,9 @@ void usb1_init(void)
 
     // initialize endpoint queue head list
     memset(queue_head_list, 0, sizeof(queue_head_list));
-    queue_head_list[0].config = (64 << 16) | (1 << 15);
-	queue_head_list[1].config = (64 << 16);
-    USB1_ENDPOINTLISTADDR = (uint32_t)&queue_head_list;
+    queue_head_list[0][0].config = (64 << 16) | (1 << 15); // RX
+	queue_head_list[0][1].config = (64 << 16); // TX
+    USB1_ENDPOINTLISTADDR = (uint32_t) &queue_head_list;
 
     USB1_USBINTR = USB_USBINTR_UE | USB_USBINTR_UEE | /* USB_USBINTR_PCE | */
 		USB_USBINTR_URE | USB_USBINTR_SLE;
@@ -114,4 +151,67 @@ void usb1_isr(void)
     // isr must acknowledge by writing bits back to the status register
     uint32_t status = USB1_USBSTS;
     USB1_USBSTS = status;
+
+    if (status & USB_USBSTS_URI) 
+    {
+        // Reset interrupt acknowledgement
+        USB1_USBSTS = USB_USBSTS_URI;
+
+        USB1_ENDPTFLUSH = 0xFFFFFFFF;  // Cancel all endpoint primed status
+        while (USB1_ENDPTFLUSH); 
+
+        // from core/teensy4
+        USB1_ENDPTSETUPSTAT = USB1_ENDPTSETUPSTAT; // Clear all setup token semaphores
+		USB1_ENDPTCOMPLETE = USB1_ENDPTCOMPLETE; // Clear all the endpoint complete status
+		while (USB1_ENDPTPRIME != 0) ; // Wait for any endpoint priming
+
+        queue_head_list[0][0].config = (64 << 16);
+        queue_head_list[0][1].config = (64 << 16);
+        USB1_DEVICEADDR = 0;
+
+        // usb_serial_reset();
+    }
+    
+    if (status & USB_USBSTS_UI) 
+    {
+        // dTD complete interrupt acknowledgement
+        USB1_USBSTS = USB_USBSTS_UI;
+
+        uint32_t setupstatus = USB1_ENDPTSETUPSTAT;
+        while (setupstatus) {
+			USB1_ENDPTSETUPSTAT = setupstatus;
+			setup_t s;
+			do {
+				USB1_USBCMD |= USB_USBCMD_SUTW;
+				s.word1 = endpoint_queue_head[0][0].setup0;
+				s.word2 = endpoint_queue_head[0][0].setup1;
+			} while (!(USB1_USBCMD & USB_USBCMD_SUTW));
+			USB1_USBCMD &= ~USB_USBCMD_SUTW;
+			//printf("setup %08lX %08lX\n", s.word1, s.word2);
+			USB1_ENDPTFLUSH = (1<<16) | (1<<0); // page 3174
+			while (USB1_ENDPTFLUSH & ((1<<16) | (1<<0))) ;
+			// endpoint0_notify_mask = 0;
+            // setup ep0 given s.bothwords
+            setup_t setup;
+            setup.bothwords = s.bothwords;
+            switch (setup.wRequestAndType) {
+                case 0x0680: // GET_DESCRIPTOR (device)
+                case 0x0681: // interface
+                
+                return;
+
+            }
+
+			setupstatus = USB1_ENDPTSETUPSTAT; // page 3175
+		}
+
+        uint32_t complete = USB1_ENDPTCOMPLETE;
+        USB1_ENDPTCOMPLETE = complete;  // Acknowledge per-endpoint completions
+
+        if (USB1_ENDPTSETUPSTAT & 0x01) 
+        {
+            memcpy(&usb_setup_packet, queue_head_list[0][0].setup, 8);
+        }
+
+    }
 }
